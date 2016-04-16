43b58ff7abeae00246cfb893b7c40fb2d37e9b72
Chris Wozniak <cwoz117@hotmail.ca>
2016-04-16 16:46:44 -0600

    closing form is a big deal

--- scripts/nav.php
+++ scripts/nav.php
@@ -3,16 +3,17 @@
 		<li><a href="index.html">Home</a></li>
 		<li><a href="games.html">Games</a></li>
 		<li><a href="admin.html">Admin</a></li>
+		
+		<li style="float:right;border-left:1px solid #0B3C5D;">
+		<a href="register.html">Register</a>
+		</li>
 		<li style="float:right;margin:7px 16px 7px 16px;">
-			<!--form action="scripts/login.php" method="post">
+			<form action="scripts/login.php" method="post">
 			Username/Password:
 			<input type="text" name="uname">
 			<input type="text" name="pword">
 			<input type="submit" value="login">
-			</form-->
-		</li>
-		<li style="float:right;border-left:1px solid #0B3C5D;">
-			<a href="register.html">Register</a>
+			</form>
 		</li>
 
 	</ul>
