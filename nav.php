ff53914a25795c383d69f8fdee8aeccdba9c41d0
Ethan Hamman <e.hamman@telus.net>
2016-04-16 20:02:26 -0600

    Login

--- nav.php
+++ nav.php
@@ -1,20 +1,25 @@
+<?php session_start(); ?>
 <nav>
 	<ul>
 		<li><a href="index.html">Home</a></li>
 		<li><a href="games.html">Games</a></li>
 		<li><a href="admin.html">Admin</a></li>
-
-		<li style="float:right;margin:7px 16px 7px 16px;">
-			<form action="login.php" method="post">
-			Username/Password:
-			<input type="text" name="uname">
-			<input type="text" name="pword">
-			<input type="submit" value="login">
-			</form>
-		</li>
-		<li style="float:right;border-left:1px solid #0B3C5D;">
-			<a href="register.html">Register</a>
-		</li>
-	
+		<?php
+		if(!isset($_SESSION['uname'])){
+			echo 'li style="float:right;margin:7px 16px 7px 16px;">
+				<form action="login.php" method="post">
+				Username/Password:
+				<input type="text" name="uname">
+				<input type="text" name="pword">
+				<input type="submit" value="login">
+				</form>
+			</li>
+			<li style="float:right;border-left:1px solid #0B3C5D;">
+				<a href="register.html">Register</a>
+			</li>';
+		}else {
+			echo '<li>' . $_SESSION['uname'] . '</li>';
+		}
+		?>
 	</ul>
 </nav>
