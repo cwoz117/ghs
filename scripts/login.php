546abeb83756fb9b6e1f78f46b3b5c158b70fc8d
root <root@localhost.localdomain>
2016-04-16 18:43:05 -0600

    form bug

--- scripts/login.php
+++ scripts/login.php
@@ -5,27 +5,26 @@
 <head>
 	<link rel="stylesheet" type="text/css" href="styles/ghs_style.css">
 	<title>Login</title>
-   <?php
-      require 'scripts/ghslib.php';
-    ?>
+	<?php require 'scripts/ghslib.php';?>
 </head>
 <body>
 	<?php require "scripts/header.php";?>
-<div class="content">
+	<div class="content">
 	<?php require "scripts/nav.php";?>
 
-   <?php
-      $dbCon = dbConnect();
-      $query = "SELECT Password FROM Users WHERE Username==$_POST["uname"]";
-      $result = $dbCon->query($query);
-      if($result==$_POST["pass"]){
-         echo "<p>Logged In</p>";
-         $_SESSION["uname"]=$_POST["uname"];
-         $_SESSION["pass"]=$_POST["pass"];
-      }else{
-         echo "<p>Not Logged In</p>";
-      }
-   ?>
+	<?php
+		$dbCon = dbConnect();
+		$query = 'SELECT Password FROM Users WHERE Username="' . $_POST['uname'] . '";';
+		$result = $dbCon->query($query);
+		$row = $result->fetch_assoc();
+		if($row['Password'] == $_POST["pass"]){
+			echo "<p>Logged In</p>";
+			$_SESSION["uname"] = $_POST["uname"];
+			$_SESSION["pass"] = $_POST["pass"];
+		}else{
+			echo "<p>Not Logged In</p>";
+		}
+	?>
 	<?php require "scripts/footer.php";?>
 </div>
 </body>
--- scripts/register.php
+++ scripts/register.php
@@ -1,28 +1,29 @@
+<?php
+echo "
 <!DOCTYPE html>
 <head>
 	<link rel="stylesheet" type="text/css" href="styles/ghs_style.css">
-	<?php require 'ghslib.php';?>
 	<title>TEMPLATE</title>
 </head>
-<body>
-	<?php require "scripts/header.php";?>
-<div class="content">
-	<?php require "scripts/nav.php";?>	
-	<?php
+<body>";
+require "scripts/header.php";
+echo "<div class="content">";
+require "scripts/nav.php";
+		require "ghslib.php";
 		$con = dbConnect();
+		
 		$uname = condition($_POST['uname']);
 		$password = condition($_POST['pass']);
 		$email = condition($_POST['email']);
 		$fname = condition($_POST['fname']);
 		$middleinit = condition($_POST['mname']);
 		$lname = condition($_POST['lname']);
-		$bday = condition($_POST['bday']);
+		//$bday = condition($_POST['bday']);
 		$address = condition($_POST['address']);
 		$phone = condition($_POST['phone']);
 		
-		$stmt = $con->prepare("INSERT INTO Users (Address, FirstName,
-				       MiddleInitial, LastName, Username, Password, Email, phoneNumber)
-				       VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);");
+		$stmt = $con->prepare("INSERT INTO Users (Address, FirstName, MiddleInitial, LastName, Username, Password, Email, phoneNumber)
+				       VALUES (?, ?, ?, ?, ?, ?, ?, ?);");
 		$stmt->bind_param("ssssssss", $address,$fname,$middleinit,$lname,$uname,$password,$email,$phone);
 		if ($stmt){
 			echo "query ok <br>";
@@ -40,11 +41,10 @@
 		
 
 		con->close();
-	?>
-
-
-
-	<?php require "scripts/footer.php";?>
+require "scripts/footer.php";
+echo "
 </div>
 </body>
 </html>
+";
+?>
